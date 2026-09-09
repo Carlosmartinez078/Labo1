5a358111342fa23db574615377fa7838b054ed80
Carlosmartinez078 <00171125@uca.edu.sv>
2026-09-09 14:36:30 -0600

    feat: agregar los menus

---
+++ labo1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(){
+    int opciones;
+
+    std::cout<<"Bienvenido al cajero: \n";
+    std::cout<<"1. Ver el Saldo" << std::endl;
+    std::cout<<"2. Depositar dinero" << std::endl;
+    std::cout<<"3. Retirar dinero" << std::endl;
+    std::cout<<"4. Salir" << std::endl;
+
+    switch (opciones) {
+    case 1:
+        
+        break;
+    
+    case 2:
+
+        break;
+
+    case 3:
+        
+        break;
+
+    case 4:
+
+        break;
+    
+    default:
+        std::cout<< "opcion invalida \n";
+        break;
+    }
+
+    return 0;
+}
